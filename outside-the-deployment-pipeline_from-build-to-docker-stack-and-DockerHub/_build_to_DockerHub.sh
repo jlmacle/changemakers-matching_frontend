ef43b1f1d9410d8ec0703e3da1568979977c1f94
jl <jeanlouis.macle@gmail.com>
2023-01-09 15:16:52 -0600

    Minor script update

--- outside-the-deployment-pipeline_from-build-to-docker-stack-and-DockerHub/_build_to_DockerHub.sh
+++ outside-the-deployment-pipeline_from-build-to-docker-stack-and-DockerHub/_build_to_DockerHub.sh
@@ -8,11 +8,11 @@ cd ..
 ng build --aot --build-optimizer --common-chunk
 
 echo "**** Moving website files in the context folder."
-cp -Rfu dist/changemakers-matchmaking_front-end/*.* outside_the_deployment_pipeline-from_build_to_docker_stack_and_DockerHub/context/html
-#cp -Rfu dist/changemakers-matchmaking_front-end/assets outside_the_deployment_pipeline-from_build_to_docker_stack_and_DockerHub/context/html
+cp -Rfu dist/changemakers-matchmaking_front-end/*.* outside-the-deployment-pipeline_from-build-to-docker-stack-and-DockerHub/context/html
+#cp -Rfu dist/changemakers-matchmaking_front-end/assets outside-the-deployment-pipeline_from-build-to-docker-stack-and-DockerHub/context/html
 
 echo "**** Building  the Docker image."
-cd outside_the_deployment_pipeline-from_build_to_docker_stack_and_DockerHub/context
+cd outside-the-deployment-pipeline_from-build-to-docker-stack-and-DockerHub/context
 sudo docker build -t jlmacle/changemakers-matchmaking-frontend:demo0 .
 sudo docker login
 sudo docker push jlmacle/changemakers-matchmaking-frontend:demo0
