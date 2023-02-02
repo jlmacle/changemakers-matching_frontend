8781d5383765267af330c0a9a406d7480119a263
jl <jeanlouis.macle@gmail.com>
2023-02-02 14:18:59 -0600

    Minor code modifications

--- cypress/e2e/Back-end-data-is-loaded.cy.ts
+++ cypress/e2e/Back-end-data-is-loaded.cy.ts
@@ -1,6 +1,5 @@
 describe('Back-end-data-loaded.cy.ts', () => {
   it('Back-end-data-loaded', () => {
-    cy.visit('http://localhost:4200').contains('test project')
-    // cy.mount()
+    cy.visit('http://localhost:4200').contains('test project');    
   })
 })
