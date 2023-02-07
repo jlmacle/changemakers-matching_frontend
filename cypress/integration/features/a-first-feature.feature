7ef6e6262e16a5bc69a054357e75cf593301fb9b
jl <jeanlouis.macle@gmail.com>
2023-02-07 16:13:18 -0600

    First test feature/step. An issue when running the code.

---
+++ cypress/integration/features/a-first-feature.feature
@@ -0,0 +1,6 @@
+Feature: I can run Cytorus
+
+Scenario: Scenario 1
+    Given A given statement
+    When A when statement
+    Then A then statement
---
+++ cypress/integration/steps/a-first-feature.js
@@ -0,0 +1,5 @@
+const { Given, When, Then, And, But, step} = require("cytorus/src/Globals");
+
+Given("A given statement", () => {})
+When("A when statement", ()=>{})
+Then("A then statement", ()=>{true.to.equal(true);})
