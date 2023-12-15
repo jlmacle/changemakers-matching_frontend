77508a0fdee12911fe53d92b6c58e07f8c83c6c3
unknown <jeanlouis.macle@gmail.com>
2023-12-15 14:12:03 -0600

    Moving code toward AngularJS

--- src/app/environments/variables.ts
+++
@@ -1,8 +0,0 @@
-export class Variables {
-
-	static baseUrl = 'http://127.0.0.1:8081'; // CORS issue for omitting http://
-	//static baseUrl = 'https://<appname>.azurewebsites.net:8080'; // Useful for configuration check.
-	// static baseUrl = 'http://<static_IP>:8080'; // Useful for configuration check.
-	
-	
-}
--- src/app/models/project.ts
+++
@@ -1,5 +0,0 @@
-export class Project {
-    // TODO: to remember the quick fix for the lack of initializer
-    id: number | undefined;
-    name: string | undefined;
-}
