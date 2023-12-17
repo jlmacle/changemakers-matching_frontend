b3ea6abfef1289f2453841158c31faedf7f00a2a
unknown <jeanlouis.macle@gmail.com>
2023-12-17 13:57:01 -0600

    Code cleanup

--- cypress/e2e/Ally-test_cypress-axe.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('Ally-test_cypress-axe.cy.ts', () => {
-  it('Ally test with cypress-axe', () => {
-    cy.visit('http://localhost:4200'); 
-    cy.injectAxe();
-    cy.checkA11y();   
-  })
-})
--- cypress/e2e/Back-end-data-is-loaded.cy.ts
+++
@@ -1,5 +0,0 @@
-describe('Back-end-data-loaded.cy.ts', () => {
-  it('Back-end-data-loaded', () => {
-    cy.visit('http://localhost:4200').contains('test project');    
-  })
-})
