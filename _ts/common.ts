90f53a0f9a397b0381e014b2b674a580ecc4ce02
unknown <jeanlouis.macle@gmail.com>
2023-12-28 14:31:21 -0600

    Worked on accessibility, code cleaning and the use of a modal

--- _ts/common.ts
+++ _ts/common.ts
@@ -16,7 +16,7 @@ function checkUsername() {
         let username_error = document.getElementById("username_error");     
         
 
-        if (username?.value && username_error) {
+        if (username?.value && username_error) {            
             if (username.value.includes("@")) {
                 if (debug) console.log("Username contains @");
                 username_error.innerHTML = "Username cannot contain @";
@@ -25,7 +25,7 @@ function checkUsername() {
             }              
             else if (username.value.includes(" ")) {
                 if (debug) console.log("Username contains an empty space");
-                username.value = username.value.trim();
+                username.value = username.value.trim(); //Suppressing the empty space
                 submit_button.disabled = true;
                 username_is_valid = false;
             }
@@ -40,6 +40,7 @@ function checkUsername() {
                 username_error.innerHTML = "The username is valid for this demo.";
                 // TODO: alphanumeric characters and underscore only. Min 3 characters, max 20 characters.                
                 username_is_valid = true;
+                // Still need to check if the password is valid before enabling the submit button.
                 if (username_is_valid && password_is_valid) submit_button.disabled = false;
             }
 
@@ -48,9 +49,10 @@ function checkUsername() {
         else {
             if (debug) console.log("(username?.value && username_error) has false for value.");
         }
-    });  
-    
+    });      
 }
+
+
 
 /**
  * Function used to check if the password is acceptable.
@@ -61,8 +63,7 @@ function checkPassword() {
     let password = document.getElementById("password") as HTMLInputElement; // To be able to access the value property
     let submit_button = document.getElementById("contributor-auth-form-submit") as HTMLButtonElement;
     password?.addEventListener("input", function(event){                
-        let password_error = document.getElementById("password_error");
-        
+        let password_error = document.getElementById("password_error");        
 
         if (password?.value && password_error) {
             if (password.value.trim().length <=8) {
@@ -73,8 +74,9 @@ function checkPassword() {
             }
             else {
                 if (debug) console.log("Password is valid");
-                password_error.innerHTML = "There is no character check in this demo.";
+                password_error.innerHTML = "This password is valid for this demo.";
                 password_is_valid = true;
+                // Still need to check if the username is valid before enabling the submit button.
                 if (username_is_valid && password_is_valid) submit_button.disabled = false;
             }
 
