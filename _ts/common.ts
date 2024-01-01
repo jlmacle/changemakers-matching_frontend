5409f682f77d5b6c41309d47be6ad57768eee4a8
unknown <jeanlouis.macle@gmail.com>
2024-01-01 13:38:06 -0600

    Pre-demo1, of better quality than demo0. Demo1 not yet ready.

--- _ts/common.ts
+++ _ts/common.ts
@@ -6,48 +6,54 @@ var password_is_valid:boolean = false;
 
 /**
  * Function used to check if the username is acceptable.
+ *  ➡️ Witnessed a case where the valid fields were empty (by selecting and suppressing), and had an issue with the submit button enabled even so.
+    ➡️ Treated at HTML level.
  */
 function checkUsername() {
     let debug = true;
 
-    let username = document.getElementById("username") as HTMLFormElement; // To be able to access the value property
+    let username = document.getElementById("username") as HTMLInputElement; 
     let submit_button = document.getElementById("auth-form-submit") as HTMLButtonElement;
-    username?.addEventListener("input", function(event){
-        let username_error = document.getElementById("username_error");     
-        
 
-        if (username?.value && username_error) {            
-            if (username.value.includes("@")) {
-                if (debug) console.log("Username contains @");
-                username_error.innerHTML = "Username cannot contain @.";
+    
+    // Typing an invalid character was not detected when being the first input, when "input" was the event.
+    // "keyup" solved the issue.
+    username?.addEventListener("keyup", function(event){
+        let username_error = document.getElementById("username_error");           
+      
+        console.log("username?.value = " + username?.value);
+        if (username?.value && username_error) {  
+           if (username.value.search(/\W/) !== -1) { // Equivalent to [^A-Za-z0-9_]
+                if (debug) console.log("Invalid character. The username can only contain unaccentuated letters, numbers and underscores.");
+                username_error.innerHTML = "Invalid character present. <br>The username can only contain unaccentuated letters, numbers and underscores.";
                 submit_button.disabled = true;
                 username_is_valid = false;
-            }              
-            else if (username.value.includes(" ")) {
-                if (debug) console.log("Username contains an empty space.");
-                username.value = username.value.trim(); //Suppressing the empty space
+            }    
+            
+            else if (username.value.length < 4) {
+                if (debug) console.log("Username is too short");
+                username_error.innerHTML = "Username should be at least 4 characters long.";
                 submit_button.disabled = true;
                 username_is_valid = false;
             }
-            else if (username.value.trim().length == 0) {
-                if (debug) console.log("Username is empty");
-                username_error.innerHTML = "Username cannot be empty.";
-                submit_button.disabled = true;
-                username_is_valid = false;
-            }           
+                      
             else {
-                if (debug) console.log("Username is valid");
-                username_error.innerHTML = "The username is valid for this demo.";
-                // TODO: alphanumeric characters and underscore only. Min 3 characters, max 20 characters.                
+                if (debug) console.log("The username is valid.");
+                username_error.innerHTML = "The username is valid.";               
                 username_is_valid = true;
                 // Still need to check if the password is valid before enabling the submit button.
                 if (username_is_valid && password_is_valid) submit_button.disabled = false;
             }
-
-
         }
         else {
-            if (debug) console.log("(username?.value && username_error) has false for value.");
+            if (debug) 
+                {
+                    console.log("(username?.value && username_error) has false for value.");
+                    console.log("username?.value = " + username?.value);
+                    console.log("username_error = " + username_error);
+                    console.log("End of : (username?.value && username_error) has false for value.");
+                }
+
         }
     });      
 }
@@ -60,20 +66,39 @@ function checkUsername() {
 function checkPassword() {
     let debug = true;
 
-    let password = document.getElementById("password") as HTMLInputElement; // To be able to access the value property
+    let password = document.getElementById("password") as HTMLInputElement; 
     let submit_button = document.getElementById("auth-form-submit") as HTMLButtonElement;
-    password?.addEventListener("input", function(event){                
-        let password_error = document.getElementById("password_error");        
+    // TODO: thorough testing of the special characters
+    // https://owasp.org/www-community/password-special-characters
+    const psswd_special_characters : string[] = [];
+
+    password?.addEventListener("keyup", function(event){                
+        let password_error = document.getElementById("password_error");  
+        
+        // "Minimum length of the passwords should be enforced by the application. 
+        // Passwords shorter than 8 characters are considered to be weak (NIST SP800-63B).
+        // Maximum password length should not be set too low, as it will prevent users from creating passphrases. 
+        // A common maximum length is 64 characters due to limitations in certain hashing algorithms"
+        // https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html  
 
         if (password?.value && password_error) {
-            if (password.value.trim().length <=8) {
+            if (password.value.length <=8) {
                 if (debug) console.log("Password is too short");
                 password_error.innerHTML = "Password should be at least 8 characters long.";
                 submit_button.disabled = true;          
                 password_is_valid = false;      
+            }        
+            else if (password.value.length >=64) {
+                if (debug) console.log("Password is too long");
+                password_error.innerHTML = "Password should be less than 64 characters long.";
+                submit_button.disabled = true;          
+                password_is_valid = false;      
             }
+            //TODO : to limit the special characters to the ones that are allowed
+            
+            
             else {
-                if (debug) console.log("Password is valid");
+                if (debug) console.log("This password is valid for this demo.");
                 password_error.innerHTML = "This password is valid for this demo.";
                 password_is_valid = true;
                 // Still need to check if the username is valid before enabling the submit button.
@@ -82,7 +107,12 @@ function checkPassword() {
 
         }
         else {
-            if (debug) console.log("(password?.value && password_error) has false for value.");
+            if (debug) {
+                console.log("(password?.value && password_error) has false for value.");
+                console.log("password?.value = " + password?.value);
+                console.log("password_error = " + password_error);
+                console.log("End of : (password?.value && password_error) has false for value.");
+            }
         }
     });
    
