62393eb362a2652866e0e5a13f5d397c4696e27b
unknown <jeanlouis.macle@gmail.com>
2024-01-02 13:29:30 -0600

    Pre-demo1, of better quality than demo0. Demo1 not yet ready.

--- _js/common.js
+++ _js/common.js
@@ -1,35 +1,31 @@
 "use strict";
 // https://code.visualstudio.com/docs/typescript/typescript-compiling
 // Ctrl+Shift+B
-var username_is_valid = false;
-var password_is_valid = false;
+let username_is_valid = false;
+let password_is_valid = false;
 /**
  * Function used to check if the username is acceptable.
- *  ➡️ Witnessed a case where the valid fields were empty (by selecting and suppressing), and the submit button was enabled.
+ *  ➡️ Witnessed a case where the valid fields were empty (by selecting and suppressing), and had an issue with the submit button enabled even so.
     ➡️ Treated at HTML level.
  */
 function checkUsername() {
     let debug = true;
-    let username = document.getElementById("username"); // To be able to access the value property
+    let username = document.getElementById("username");
     let submit_button = document.getElementById("auth-form-submit");
-    // Typing an invalid character was not detected when being the first input, when input was the event.
+    // Typing an invalid character was not detected when being the first input, when "input" was the event.
+    // "keyup" solved the issue.
     username === null || username === void 0 ? void 0 : username.addEventListener("keyup", function (event) {
         let username_error = document.getElementById("username_error");
-        // "Minimum length of the passwords should be enforced by the application. 
-        // Passwords shorter than 8 characters are considered to be weak (NIST SP800-63B).
-        // Maximum password length should not be set too low, as it will prevent users from creating passphrases. 
-        // A common maximum length is 64 characters due to limitations in certain hashing algorithms"
-        // https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html        
-        console.log("keyup: username?.value = " + (username === null || username === void 0 ? void 0 : username.value));
+        console.log("username?.value = " + (username === null || username === void 0 ? void 0 : username.value));
         if ((username === null || username === void 0 ? void 0 : username.value) && username_error) {
             if (username.value.search(/\W/) !== -1) { // Equivalent to [^A-Za-z0-9_]
                 if (debug)
-                    console.log("Invalid character. The password can only contain unaccentuated letters, numbers and underscores.");
-                username_error.innerHTML = "Invalid character present. <br>The password can only contain unaccentuated letters, numbers and underscores.";
+                    console.log("Invalid character. The username can only contain unaccentuated letters, numbers and underscores.");
+                username_error.innerHTML = "Invalid character present. <br>The username can only contain unaccentuated letters, numbers and underscores.";
                 submit_button.disabled = true;
                 username_is_valid = false;
             }
-            else if (username.value.trim().length < 4) {
+            else if (username.value.length < 4) {
                 if (debug)
                     console.log("Username is too short");
                 username_error.innerHTML = "Username should be at least 4 characters long.";
@@ -61,25 +57,27 @@ function checkUsername() {
  */
 function checkPassword() {
     let debug = true;
-    let password = document.getElementById("password"); // To be able to access the value property
+    let password = document.getElementById("password");
     let submit_button = document.getElementById("auth-form-submit");
     // TODO: thorough testing of the special characters
     // https://owasp.org/www-community/password-special-characters
     const psswd_special_characters = [];
-    password === null || password === void 0 ? void 0 : password.addEventListener("input", function (event) {
+    password === null || password === void 0 ? void 0 : password.addEventListener("keyup", function (event) {
         let password_error = document.getElementById("password_error");
+        // "Minimum length of the passwords should be enforced by the application. 
+        // Passwords shorter than 8 characters are considered to be weak (NIST SP800-63B).
+        // Maximum password length should not be set too low, as it will prevent users from creating passphrases. 
+        // A common maximum length is 64 characters due to limitations in certain hashing algorithms"
+        // https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html  
         if ((password === null || password === void 0 ? void 0 : password.value) && password_error) {
-            if (password.value.trim().length <= 8) {
+            if (password.value.length <= 8) {
                 if (debug)
                     console.log("Password is too short");
                 password_error.innerHTML = "Password should be at least 8 characters long.";
                 submit_button.disabled = true;
                 password_is_valid = false;
             }
-            // Maximum password length should not be set too low, as it will prevent users from creating passphrases. 
-            // A common maximum length is 64 characters due to limitations in certain hashing algorithms
-            // https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html
-            else if (password.value.trim().length >= 64) {
+            else if (password.value.length >= 64) {
                 if (debug)
                     console.log("Password is too long");
                 password_error.innerHTML = "Password should be less than 64 characters long.";
